5c3978dab70b8cedda87d6f8c49a7d93299c7c02
mohammadmurtuza <62512039+mohammadmurtuza@users.noreply.github.com>
2024-02-05 17:00:01 -0500

    HW3 DONE
    Part of the code for HW3 done main project still pending

--- Extras/sample.py
+++ Extras/sample.py
@@ -1,23 +1,39 @@
-from road import Dynamic
-from constants import Constants
+class Constants:
+    AccRate = 3.5  # Acceleration rate for cars in m/s
+    AccRateEmpty = 2.5  # Acceleration rate for light trucks in m/s
+    AccRateFull = 1.0  # Acceleration rate for heavy trucks in m/s
+    DecRate = 7.0  # Braking rate for cars in m/s
+    DecRateEmpty = 5.0  # Braking rate for light trucks in m/s
+    DecRateFull = 2.0  # Braking rate for light trucks in m/s
+    MpsToMph = 2.237
 
+class Program:
+    @staticmethod
+    def main():
+        car = Car()
+        car.set_desired_speed(65.0)
+        truck1 = Truck(4)
+        truck1.set_desired_speed(55.0)
+        truck2 = Truck(8)
+        truck2.set_desired_speed(50.0)
+        vehicles = [car, truck1, truck2]
+        for _ in range(11):
+            for v in vehicles:
+                v.update_speed(1)
+                s = v.__class__.__name__
+                print(f"{s} speed: {v.get_current_speed():.2f} mph")
 
-class Vehicle(Dynamic):
+class Vehicle:
     def __init__(self):
-        super().__init__()
-        self.current_speed = 0
-        self.desired_speed = 0
-        self.speedLimit = 0
-        self.color = None 
-        self.currentDirection = 0
-        self.currentLocation = (0, 0)
+        self.current_speed = 0.0
+        self.desired_speed = 0.0
 
     def get_current_speed(self):
         return self.current_speed
 
     def set_desired_speed(self, mph):
         self.desired_speed = mph
-    
+
     def set_current_speed(self, speed):
         if self.current_speed <= speed:  # accelerating
             if speed > self.desired_speed:
@@ -29,13 +45,12 @@ class Vehicle(Dynamic):
                 self.current_speed = self.desired_speed
             else:
                 self.current_speed = speed
-    
+
     def update_speed(self, seconds):
         if self.current_speed > self.desired_speed:
             self.decelerate(seconds)
         elif self.current_speed < self.desired_speed:
             self.accelerate(seconds)
-      
 
     def accelerate(self, seconds_delta):
         pass
@@ -43,15 +58,7 @@ class Vehicle(Dynamic):
     def decelerate(self, seconds_delta):
         pass
 
-    def Turn(self, direction, degrees):
-        #print(f"Turning {direction} {degrees}°") # for debugging purposes
-        pass
-
-
 class Car(Vehicle):
-    def __init__(self):
-        super().__init__() #The super() function is used to give access to methods and properties of a parent or sibling class.
-
     def accelerate(self, seconds_delta):
         self.set_current_speed(self.get_current_speed() + Constants.AccRate * seconds_delta * Constants.MpsToMph)
 
@@ -59,25 +66,21 @@ class Car(Vehicle):
         self.set_current_speed(self.get_current_speed() - Constants.DecRate * seconds_delta * Constants.MpsToMph)
 
 class Truck(Vehicle):
-    def __init__(self,weight):
+    def __init__(self, weight):
         super().__init__()
-        self.loadWeight = weight
-
-    def SetLoadWeight(self, weight):
-        #print(f"Setting Truck load to {weight} lbs.") # for debugging purposes
-        pass
+        self.load_weight = weight
 
     def accelerate(self, seconds_delta):
-        if self.loadWeight <= 5:
+        if self.load_weight <= 5:
             self.set_current_speed(self.get_current_speed() + Constants.AccRateEmpty * seconds_delta * Constants.MpsToMph)
         else:
             self.set_current_speed(self.get_current_speed() + Constants.AccRateFull * seconds_delta * Constants.MpsToMph)
 
     def decelerate(self, seconds_delta):
-        if self.loadWeight <= 5:
+        if self.load_weight <= 5:
             self.set_current_speed(self.get_current_speed() - Constants.DecRateEmpty * seconds_delta * Constants.MpsToMph)
         else:
             self.set_current_speed(self.get_current_speed() - Constants.DecRateFull * seconds_delta * Constants.MpsToMph)
 
-
-
+# Run the main function
+Program.main()
--- program.py
+++ program.py
@@ -1,20 +1,20 @@
 # program.py
 from vehicles import Car, Truck
-from constants import Constants
+
 class Program:
+    @staticmethod
     def main():
         car = Car()
-        car.SetDesiredSpeed(65.0)
+        car.set_desired_speed(65.0)
         truck1 = Truck(4)
-        truck1.SetDesiredSpeed(55.0)
+        truck1.set_desired_speed(55.0)
         truck2 = Truck(8)
-        truck2.SetDesiredSpeed(50.0)
+        truck2.set_desired_speed(50.0)
         vehicles = [car, truck1, truck2]
-
         for _ in range(11):
             for v in vehicles:
                 v.update_speed(1)
                 s = v.__class__.__name__
-                print(f"{s} speed: {v.GetCurrentSpeed():.2f} mph")
+                print(f"{s} speed: {v.get_current_speed():.2f} mph")
 
 Program.main()
